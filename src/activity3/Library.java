c40e6b9aca0842bda1094688d9fd3b3a742ce2f6
prmr <prmr@gmx.com>
2020-10-06 20:42:31 -0400

    Add more scaffolding

--- src/activity3/Library.java
+++ src/activity3/Library.java
@@ -3,6 +3,9 @@ package activity3;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a collection of songs, playlists, and albums.
+ */
 public class Library
 {
 	public static final Library INSTANCE = new Library();
--- src/activity3/Song.java
+++ src/activity3/Song.java
@@ -5,12 +5,11 @@ import java.util.Map;
 
 public class Song
 {
+	private final String aFile;
 	private String aTitle;
 	private int aDuration;
-	private final String aFile;
 	private Artist aArtist;
 	private String aGenre;
-	
 	private Map<String, String> aTags = new HashMap<>();
 	
 	public Song(String pFile)
